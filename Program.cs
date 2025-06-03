f39e25d9fdd86a828abd175ef70c696924a71a63
Wanchaloem Laoket <TTVone@hotmail.com>
2025-06-03 18:09:50 +0700

    Update Short Files

--- Program.cs
+++ Program.cs
@@ -1,13 +1,7 @@
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateSlimBuilder(args);
-
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
-
-var app = builder.Build();
+builder.Services.ConfigureHttpJsonOptions(options => options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default));
 
 var sampleTodos = new Todo[] {
     new(1, "Walk the dog"),
@@ -17,19 +11,12 @@ var sampleTodos = new Todo[] {
     new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
 };
 
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos);
-todosApi.MapGet("/{id}", (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? Results.Ok(todo)
-        : Results.NotFound());
-
+var app = builder.Build();
+app.MapGet("/", () => sampleTodos);
+app.MapGet("/{id}", (int id) => sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo ? todo : null);
 app.Run();
 
-public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-
 [JsonSerializable(typeof(Todo[]))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
+internal partial class AppJsonSerializerContext : JsonSerializerContext { }
 
-}
+public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
